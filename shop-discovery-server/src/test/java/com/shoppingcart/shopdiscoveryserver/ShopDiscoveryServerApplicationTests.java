4493055c64a7b5a37215925bec8409633de0b634
Suruchi Sinha <suruchisinha.3011@gmail.com>
2022-07-22 15:51:48 +0530

    text-formatting uploaded

---
+++ shop-discovery-server/src/test/java/com/shoppingcart/shopdiscoveryserver/ShopDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.shopdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
